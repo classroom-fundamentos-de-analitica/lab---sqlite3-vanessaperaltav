ecd1428f9e2a0722e778db1000e94971a9d28591
vanessaperaltav <125827890+vanessaperaltav@users.noreply.github.com>
2023-06-13 18:29:49 -0500

    Update pregunta_07.sql

--- pregunta_07.sql
+++ pregunta_07.sql
@@ -41,5 +41,4 @@
 --  11  C  19  570.43  400  2019-04-12  0.48  FBEE
 --  12  C  24  482.32  300  2019-05-03  0.11  CCCE
 -- 
---  >>> Escriba su codigo a partir de este punto <<<
---
+SELECT * FROM tbl1 WHERE K0 <> 'A' AND K0 <> 'B' AND c13 <> 200 AND c13 <> 900 ORDER BY c14
