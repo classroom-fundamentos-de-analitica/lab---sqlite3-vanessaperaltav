5232461d34371895310b52d6ec700aeeb92b76a8
vanessaperaltav <125827890+vanessaperaltav@users.noreply.github.com>
2023-06-13 18:28:27 -0500

    Update pregunta_04.sql

--- pregunta_04.sql
+++ pregunta_04.sql
@@ -27,10 +27,7 @@
 --    c23 DATE,
 --    c24 FLOAT,
 --    c25 CHAR(5)
---
---  Escriba una consulta que retorne los campos K0 y c16
---  para los registros de la tabla tbl1 para los que la 
---  columna c16 empieza por la misma letra de la columna K0.
+SELECT k0, c16 FROM tbl1 WHERE SUBSTR(k0,1,1) = SUBSTR(c16,1,1);
 --
 --  Rta/
 --    K0   c16
