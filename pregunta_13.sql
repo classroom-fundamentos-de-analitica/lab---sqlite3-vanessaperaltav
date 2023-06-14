c6135d9f8e5e7586bdabccfbbcab2c8c4e6a058e
vanessaperaltav <125827890+vanessaperaltav@users.noreply.github.com>
2023-06-13 19:12:00 -0500

    Update pregunta_13.sql

--- pregunta_13.sql
+++ pregunta_13.sql
@@ -28,19 +28,19 @@
 --    c24 FLOAT,
 --    c25 CHAR(5)
 --
---  Escriba una consulta que retorne por cada letra de la
---  columna K0 de la tabla tbl1, el valor promedio de la 
---  columna c21 en la tabla tbl2, para aquellos registros
---  con c13 mayor a 400.
+--  Escriba una consulta que retorne por cada letra
+--  de la columna K0, el valor promedio de la columna c12
+--  en la tabla tbl1, para aquellos registros con c13 mayor
+--  a 400.
 --
 --  Rta/
---  K0    avg(c21)
---  0  A  593.495000
---  1  B  575.470000
---  2  C  530.753000
---  3  D  655.612500
---  4  E  555.323077
---
+--    K0    avg(c12)
+--  0  A  476.155000
+--  1  B  536.523333
+--  2  C  490.830000
+--  3  D  709.530000
+--  4  E  474.825000
+-- 
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
-SELECT K0, avg(c21) FROM tbl1 INNER JOIN tbl2 ON tbl1.K1 = tbl2.K1 WHERE c13 > 400 GROUP BY K0 ORDER BY K0;
+SELECT K0, avg(c12) FROM tbl1 WHERE c13 > 400 GROUP BY K0 ORDER BY K0;
